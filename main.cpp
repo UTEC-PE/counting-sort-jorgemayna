b91bf2d7a4527321be526190b65204a4a331bfd2
jorgemayna <jorgemayna@gmail.com>
2018-09-18 22:00:31 -0500

    gg

--- main.cpp
+++ main.cpp
@@ -13,7 +13,7 @@ int main(int argc, char *argv[]) {
     print(numbers, size);
     countingSort(numbers, size);
     print(numbers, size);
-    
+
     system("read");
     return EXIT_SUCCESS;
 }
@@ -26,5 +26,40 @@ void print(int numbers[], int size) {
 }
 
 void countingSort(int numbers[], int size) {
-    // TODO
+
+    int menor=numbers[0];
+    int mayor=numbers[0];
+    int re[size];
+    for (int i=0; i<size; i++){
+        if (numbers[i]> mayor){
+        mayor=numbers[i];
+        }
+        if (numbers[i]< menor){
+        menor=numbers[i];
+        }
+    }
+    const int t=mayor-menor+1;
+    int a[t]={0};
+    for (int i = 0; i < size; i++)
+    {
+        a[numbers[i]-menor]++;
+
+    }
+    int cc=0;
+    for (int i = 0; i < t; i++)
+    {
+        a[i]=cc+a[i];
+        cc=a[i];
+
+    }
+    for (int i = 0; i < size; i++)
+    {
+        re[a[numbers[i]-menor]-1]=numbers[i];
+        a[numbers[i]-menor]--;
+    }
+    for (int i = 0; i < size; i++)
+    {
+        numbers[i]=re[i];
+    }
+
 }
